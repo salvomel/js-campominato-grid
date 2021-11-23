965c0a137017ef7d563b0ef00019686400d68b71
salvomel <salv.mel@gmail.com>
2021-11-23 16:47:55 +0100

    function click to play

--- js/script.js
+++ js/script.js
@@ -5,36 +5,39 @@
 // con difficoltà 3 => tra 1 e 49
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
+// Funzione CLICK to PLAY
+document.getElementById('play').addEventListener('click', startGame);
+function startGame() {
 
-// CREO LA GRIGLIA CON GLI SQUARE
-// Array di numeri da 1 a 100
-const numberOfSquares = 100;
-
-// Richiamo funzione Genera Numeri nell Griglia
-let generatedNumbers = generateGridNumbers(numberOfSquares);
-// Ordino i numeri in modo ascendente
-generatedNumbers.sort(function(a, b){return a-b});
-
-// Per ogni numero nell'array creo una cella e la appendo al grid
-const myGrid = document.getElementById('grid');
-myGrid.innerHTML = '';
-
-for(let i = 0; i < generatedNumbers.length; i++) {
+    // CREO LA GRIGLIA CON GLI SQUARE
+    // Array di numeri da 1 a 100
+    const numberOfSquares = 100;
     
-    const thisNumber = generatedNumbers[i];
-    const newSquare = generateGridItem(thisNumber);
-
-    // Evento CLICK
-    newSquare.addEventListener('click', squareClick);
-     
-    // Aggiungo l'elemento alla griglia
-    myGrid.appendChild(newSquare);
+    // Richiamo funzione Genera Numeri nell Griglia
+    let generatedNumbers = generateGridNumbers(numberOfSquares);
+    
+    // Per ogni numero nell'array creo una cella e la appendo al grid
+    const myGrid = document.getElementById('grid');
+    myGrid.innerHTML = '';
+    
+    for(let i = 0; i < generatedNumbers.length; i++) {
+        
+        const thisNumber = generatedNumbers[i];
+        const newSquare = generateGridItem(thisNumber);
+    
+        // Evento CLICK
+        newSquare.addEventListener('click', squareClick);
+         
+        // Aggiungo l'elemento alla griglia
+        myGrid.appendChild(newSquare);
+    }
+    
+    // AL CLICK SU OGNI SQUARE AGGIUNGO LA CLASSE "ACTIVE" ALLO SQUARE
+    function squareClick() {
+        this.classList.add('active');
+    }
 }
 
-// AL CLICK SU OGNI SQUARE AGGIUNGO LA CLASSE "ACTIVE" ALLO SQUARE
-function squareClick() {
-    this.classList.add('active');
-}
 
 // _____________
 // | FUNCTIONS |
@@ -46,13 +49,8 @@ function squareClick() {
 function generateGridNumbers (quantityOfNumbers) {
     const numbersArray = [];
 
-    while(numbersArray.length < quantityOfNumbers) {
-
-        const randomNumber = getRndInteger(1, quantityOfNumbers);
-        // If per non ripetizione di numeri e pusho nell'Array
-        if( !numbersArray.includes(randomNumber) ) {
-            numbersArray.push(randomNumber);
-        }
+    for ( let i= 1; i < quantityOfNumbers + 1; i++) {
+         numbersArray.push(i);
     }
     return numbersArray;
 }
@@ -67,10 +65,5 @@ function generateGridItem(number) {
     newSquare.innerHTML = `<span>${number}</span>`; 
 
     return newSquare;
-}
-
-// Funzione genera numeri random
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
